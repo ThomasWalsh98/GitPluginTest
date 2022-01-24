46536e3977e3cebb609b28ce8c1896cf6c59b233
ThomasWalsh98 <72092621+ThomasWalsh98@users.noreply.github.com>
2022-01-24 15:38:52 +0000

    Committing to the new branch

--- src/packt/jee/eclipse/git/GitTestApplication.java
+++ src/packt/jee/eclipse/git/GitTestApplication.java
@@ -5,6 +5,7 @@ public class GitTestApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello world, I'M GOING TO PUT THIS IN A GIT REPOSITORY!, OOPS");
 		System.out.println("My bad wasn't meant to capitaise it");
+		System.out.println("Hello branch #bug1234!");
 
 	}
 
