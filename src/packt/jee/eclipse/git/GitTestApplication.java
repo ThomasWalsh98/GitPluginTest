052d76d303203e5fd290d98ab8c48bf67f915f03
ThomasWalsh98 <72092621+ThomasWalsh98@users.noreply.github.com>
2022-01-24 15:10:14 +0000

    Initial commit

---
+++ src/packt/jee/eclipse/git/GitTestApplication.java
@@ -0,0 +1,10 @@
+package packt.jee.eclipse.git;
+
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world, I'M GOING TO PUT THIS IN A GIT REPOSITORY!, oops");
+
+	}
+
+}
