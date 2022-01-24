b300b89889326d416837e24e38d17e125f961d5a
ThomasWalsh98 <72092621+ThomasWalsh98@users.noreply.github.com>
2022-01-24 15:19:37 +0000

    changes made to file

--- src/packt/jee/eclipse/git/GitTestApplication.java
+++ src/packt/jee/eclipse/git/GitTestApplication.java
@@ -3,7 +3,8 @@ package packt.jee.eclipse.git;
 public class GitTestApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world, I'M GOING TO PUT THIS IN A GIT REPOSITORY!, oops");
+		System.out.println("Hello world, I'M GOING TO PUT THIS IN A GIT REPOSITORY!, OOPS");
+		System.out.println("My bad wasn't meant to capitaise it");
 
 	}
 
